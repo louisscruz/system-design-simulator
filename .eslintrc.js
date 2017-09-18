def67d5ec5267dd9b82d1c5c02428895b042269e
louisscruz <louisstephancruz@me.com>
2017-09-17 23:43:41 -0700

    Remove global-require

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
   "rules": {
     "jsx-a11y/href-no-hash": "off",
     "jsx-a11y/anchor-is-valid": ["warn", { "aspects": ["invalidHref"] }],
-    "react/forbid-prop-types": [0]
+    "react/forbid-prop-types": [0],
+    "global-require": [0]
   }
 };
